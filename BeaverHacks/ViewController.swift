39dac0e17b38510c36bcf8f52f2d94f64dad8187
Ryan <rgrossry@oregonstate.edu>
2019-05-27 15:59:55 -0400

    Initial Commit

---
+++ BeaverHacks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeaverHacks
+//
+//  Created by Ryan Gross on 5/27/19.
+//  Copyright © 2019 OSUHackathonClub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
